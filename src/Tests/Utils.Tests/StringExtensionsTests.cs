b2243b4a6f85af29e3859d7dd766baa4715a9521
Andrey Suvorov <spoofi.lab@gmail.com>
2015-08-30 16:37:54 +0500

    Utils.Tests project

---
+++ src/Tests/Utils.Tests/StringExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using Spoofi.FreudBot.Utils.Extensions;
+using Xunit;
+
+namespace Spoofi.FreudBot.Utils.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void HasValueTest()
+        {
+            Assert.False("".HasValue());
+            Assert.False(" ".HasValue());
+            Assert.False(((string)null).HasValue());
+            Assert.True("string".HasValue());
+        }
+    }
+}
